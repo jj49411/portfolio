800f1f41897fcd32efddaf37c3af62aec6569de7
Julie <j49411@gmail.com>
2020-05-07 18:55:10 +0100

    test: render about content and links on homepage

--- src/components/__tests__/HomeAbout.test.js
+++ src/components/__tests__/HomeAbout.test.js
@@ -24,4 +24,26 @@ describe('HomeAbout', () => {
     expect(wrapper.find(`[data-test='profile-img']`).length).toBe(1)    
   })
 
+  it('should render about content from props', () => {
+    const props = {
+      titleA: 'About Me',
+      text1: 'My background...',
+      text2: 'The reason I love coding...',
+    }
+    wrapper = shallow(<HomeAbout {...props}/>)
+    expect(wrapper.find('.homeAbout').text()).toBe('About Me')  
+  })
+
+  it('should have a link to projects page', () => {
+    expect(wrapper.find('.projectsLink').length).toBe(1)    
+  })
+
+  it('should show Linkedin link', () => {
+    expect(wrapper.find('.icon').length).toBe(1)    
+  })
+
+  it('should show Github link', () => {
+    expect(wrapper.find('.icon2').length).toBe(1)    
+  })
+
 })
